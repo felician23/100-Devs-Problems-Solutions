3e6c6a02529cd92661419853d5e910565c1ad0c3
Davon Daniels <38470284+felician23@users.noreply.github.com>
2025-07-30 22:10:55 -0500

    Add files via upload

---
+++ MightyMorphin.js
@@ -0,0 +1,21 @@
+let myfav = "Halloween";
+myfav = myfav.toUpperCase();
+console.log(myfav);
+
+let myArr = "welcome"
+alert(myArr.slice(-3))
+
+function takesInFive(a, b, c, d, e) {
+    return Math.abs(a - b - c - d - e - 100)
+}
+
+takesInFive()
+
+function takesInThree(a, b, c) {
+    console.log(Math.min(a,b,c))
+    console.log(Math.max(a,b,c))
+}
+
+function headsTails(a) {
+    
+}
